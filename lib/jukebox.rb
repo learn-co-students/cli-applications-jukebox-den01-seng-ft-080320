c1a08fad47888537a485c3b8b36ae7c2b97cdc4f
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-31 17:34:40 +0000

    Done.

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,65 @@
+require 'pry'
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 # Add your code here
+def run(songs)
+puts "Please enter a command:"
+user_input = gets.strip
+while user_input.downcase != "exit"
+  if user_input.downcase == "help"
+   help
+  elsif user_input.downcase == "play"
+    play(songs)
+  elsif user_input.downcase == "list"
+    list(songs)
+  elsif user_input.downcase == "exit"
+    exit_jukebox
+end
+end
+exit_jukebox
+end
+
+
+def help
+    #output instructions
+    puts "I accept the following commands:"
+    puts "- help : displays this help message"
+    puts "- list : displays a list of songs you can play"
+    puts "- play : lets you choose a song to play"
+    puts "- exit : exits this program"
+end
+
+def play(songs)
+  #ask input a song name OR track number
+  puts "Please enter a song name or number:"
+  song_input = gets.strip
+  songs.each_with_index do |index, value|
+    if song_input == (index + 1) or songs[value]
+      puts "Playing #{value}"
+    else
+      puts "Invalid input, please try again."
+    end
+  end
+end
+
+def list(songs)
+  #output song list
+  songs.each_with_index do |index, songs|
+     puts "#{index + 1}. #{songs[index]}"
+   end
+end
+
+def exit_jukebox
+    puts "Goodbye"
+end
